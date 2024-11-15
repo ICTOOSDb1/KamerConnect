a2c741a9d09ab3e5c766468bf2e7c4d9c37c870a
samberkhout <s1199368@student.windesheim.nl>
2024-11-15 11:39:13 +0100

    Revert "style: change collor and frame"
    This reverts commit 95a852366fc5f3b2916f5a665e41ef006ee1b8c2.

--- applicatie/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace applicatie;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
