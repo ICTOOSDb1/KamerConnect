6c585d819f46540b95401f145e330b3c4baf3a16
niekvandenberg <niek2004@icloud.com>
2024-11-16 16:07:07 +0100

    style: added reusable components to the login page

---
+++ KamerConnect/Services/AuthenticationService.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using KamerConnect.Models;
+
+namespace KamerConnect;
+
+public class AuthenticationService
+{
+
+    public Person Person { get; set; }
+
+    private void authenticate(Person person)
+    {
+        //do sql call to check user
+    }
+    
+    public static bool IsValidEmail(string email)
+    {
+        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+        Regex regex = new Regex(pattern);
+        return regex.IsMatch(email);
+    }
+}
