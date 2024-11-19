6720998636a320cbc330716859fd5226a9a8c0b5
RenzeBoerman <mail@renzeboerman.nl>
2024-11-19 19:10:42 +0100

    chore: remove test code

--- KamerConnect.View.MAUI/MauiProgram.cs
+++ KamerConnect.View.MAUI/MauiProgram.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using KamerConnect.EnvironmentVariables;
-using KamerConnect.Repositories;
-using KamerConnect.DataAccess.Minio;
 
 namespace KamerConnect.View.MAUI;
 
@@ -10,18 +8,6 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		EnvVariables.Load();
-		IFileRepository fileRepository = new FileRepository();
-
-		Task.Run(async () =>
-		{
-
-			string bucketName = "mijn-bucket";
-			string objectName = "dummy-bestand.txt";
-			byte[] fileBytes = System.Text.Encoding.UTF8.GetBytes("Dit is een dummy bestand dat geüpload wordt naar MinIO.");
-			string contentType = "text/plain";
-
-			await fileRepository.UploadFileAsync(bucketName, objectName, fileBytes, contentType);
-		});
 
 		var builder = MauiApp.CreateBuilder();
 		builder
