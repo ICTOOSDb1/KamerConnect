f4bc8dfe1320e172ff79db03ce53c5aa38c5d014
MatthiasvanB <matthias.van.beuzekom@windesheim.nl>
2024-11-25 18:48:31 +0100

    .

--- KamerConnect.View.MAUI/App.xaml.cs
+++ KamerConnect.View.MAUI/App.xaml.cs
@@ -1,38 +1,35 @@
-
-﻿using KamerConnect.DataAccess.Postgres.Repositys;
+using KamerConnect.DataAccess.Postgres.Repositys;
 using KamerConnect.Repositories;
 using KamerConnect.Services;
 using KamerConnect.View.MAUI.Pages;
 
 namespace KamerConnect.View.MAUI;
 
-
 public partial class App : Application
 {
-	public App()
-	{
-		this.serviceProvider = serviceProvider;
-		InitializeComponent();
+    private IServiceProvider serviceProvider;
+    public App(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+        InitializeComponent();
+        _ = InitializeAppAsync(); // Fire-and-forget the async initialization
+    }
 
-		_ = InitializeAppAsync(); // Fire-and-forget the async initialization
-	}
-	
-	private async Task InitializeAppAsync()
-	{
-		var authService = serviceProvider.GetService<AuthenticationService>();
-		if (authService == null)
-		{
-			return;
-		}
-    
-		if (await authService.CheckSession())
-		{
-			MainPage = new NavigationPage(new MainPage());
-		}
-		else
-		{
-			MainPage = new NavigationPage(serviceProvider.GetRequiredService<LoginPage>());
-		}
+    private async Task InitializeAppAsync()
+    {
+        var authService = serviceProvider.GetService<AuthenticationService>();
+        if (authService == null)
+        {
+            return;
+        }
 
-	}
+        if (await authService.CheckSession())
+        {
+            MainPage = new NavigationPage(new MainPage());
+        }
+        else
+        {
+            MainPage = new NavigationPage(serviceProvider.GetRequiredService<LoginPage>());
+        }
+    }
 }
