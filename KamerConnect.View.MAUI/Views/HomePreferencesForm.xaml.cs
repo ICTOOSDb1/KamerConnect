8f2dbb07ed86d1a3535da40fa80c0ef35e2547b6
niekvandenberg <niek2004@icloud.com>
2024-11-26 17:40:45 +0100

    fix: added homepreferencespages

---
+++ KamerConnect.View.MAUI/Views/HomePreferencesForm.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamerConnect.View.MAUI.Views;
+
+public partial class HomePreferencesForm : ContentView
+{
+    public HomePreferencesForm()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
