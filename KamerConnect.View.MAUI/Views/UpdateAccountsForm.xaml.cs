41d60f398584e9d68717c6001cb003a5d66d1e34
RenzeBoerman <mail@renzeboerman.nl>
2024-11-26 16:22:09 +0100

    chore: remove unused duplicate function

--- KamerConnect.View.MAUI/Views/UpdateAccountsForm.xaml.cs
+++ KamerConnect.View.MAUI/Views/UpdateAccountsForm.xaml.cs
@@ -58,22 +58,6 @@ public partial class UpdateAccountsForm : ContentView
         _personService.UpdatePerson(_currentPerson);
     }
 
-
-    private string GetContentType(string fileName)
-    {
-        var extension = Path.GetExtension(fileName)?.ToLowerInvariant();
-
-        return extension switch
-        {
-            ".png" => "image/png",
-            ".jpg" or ".jpeg" => "image/jpeg",
-            ".gif" => "image/gif",
-            ".bmp" => "image/bmp",
-            ".tiff" => "image/tiff",
-            _ => "application/octet-stream"
-        };
-    }
-    
     public bool ValidateForm()
     {
         firstNameEntry.Validate();
