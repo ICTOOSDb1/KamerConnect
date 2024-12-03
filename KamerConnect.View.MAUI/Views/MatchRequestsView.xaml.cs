6b17580ba27bd96d61389df972bc8a8276b0b16d
MatthiasvanB <matthias.van.beuzekom@windesheim.nl>
2024-12-03 15:24:22 +0100

    grid wordt m

--- KamerConnect.View.MAUI/Views/MatchRequestsView.xaml.cs
+++ KamerConnect.View.MAUI/Views/MatchRequestsView.xaml.cs
@@ -8,8 +8,26 @@ namespace KamerConnect.View.MAUI.Views;
 
 public partial class MatchRequestsView : ContentView
 {
+    
     public MatchRequestsView()
     {
         InitializeComponent();
+        GetMatchRequests();
+    }
+
+    public void GetMatchRequests()
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            var label = new Label { Text = $"Label {i} 1" };
+            var label1 = new Label { Text = $"Label {i} 2"};
+            var label2 = new Label { Text = $"Label {i} 3"};
+            var label3 = new Label { Text = $"Label {i} 4"};
+            MatchRequests.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100, GridUnitType.Absolute) });
+            MatchRequests.Add(label, i, 0);
+            MatchRequests.Add(label1, i, 1);
+            MatchRequests.Add(label2, i, 2);
+            MatchRequests.Add(label3, i, 3);
+        }
     }
 }
