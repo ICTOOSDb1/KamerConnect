461ba80af3e1bacb8e3f0e7b9f80f8f6652ce014
Aernout5 <aernout.bergsma@live.nl>
2024-12-07 13:52:25 +0100

    opmaak verbeterd

--- KamerConnect.View.MAUI/Views/MatchRequestsView.xaml.cs
+++ KamerConnect.View.MAUI/Views/MatchRequestsView.xaml.cs
@@ -33,10 +33,12 @@ public partial class MatchRequestsView : ContentView
         GetCurrentPerson().GetAwaiter().GetResult();
         if (_person.Role == Role.Offering)
         {
-           GetMatchRequestsOffering(); 
+            AddLegend("Voornaam", "School", "Opleiding","Geboortedatum");
+            GetMatchRequestsOffering(); 
         }
         else
         {
+            AddLegend("Straat", "Stad", "Type","Prijs");
             GetMatchRequestsSeeking();
         }
         
@@ -81,18 +83,21 @@ public partial class MatchRequestsView : ContentView
                     HorizontalOptions = LayoutOptions.Center
                 }
             };
-            var label1 = new Label { Text = person.FirstName, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
-            var label2 = new Label { Text = (person.Personality != null) ? person.Personality.School : "" , HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
-            var label3 = new Label { Text = (person.Personality!= null) ? person.Personality.Study : "", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
-            var label4 = new Label { Text = person.BirthDate.ToShortDateString(), HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
+            var separator = CreateSeparator();
+            var label1 = new Label { Text = person.FirstName, FontFamily = "OpenSansRegular", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
+            var label2 = new Label { Text = (person.Personality != null) ? person.Personality.School : "" , FontFamily = "OpenSansRegular", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
+            var label3 = new Label { Text = (person.Personality!= null) ? person.Personality.Study : "", FontFamily = "OpenSansRegular", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
+            var label4 = new Label { Text = person.BirthDate.ToShortDateString(), FontFamily = "OpenSansRegular", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
             MatchRequests.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100, GridUnitType.Absolute) });
+            MatchRequests.Add(separator, 0, i);
+            Grid.SetColumnSpan(separator, 6);
             MatchRequests.Add(border, 0, i);
             MatchRequests.Add(label1, 1, i);
             MatchRequests.Add(label2, 2, i);
             MatchRequests.Add(label3, 3, i);
             MatchRequests.Add(label4, 4, i);
+            DisplayStatus(i, matches[i-1].Status);
         }
-        AddLegend("Voornaam", "School", "Opleiding","Geboortedatum");
     }
 
     public void GetMatchRequestsSeeking()
@@ -139,51 +144,101 @@ public partial class MatchRequestsView : ContentView
                     houseTypeTranslation = "Studio";
                     break;
             }
-            var label1 = new Label { Text = house.Street, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
-            var label2 = new Label { Text = house.City, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
-            var label3 = new Label { Text = houseTypeTranslation, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
-            var label4 = new Label { Text = "€"+house.Price, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
+
+            var separator = CreateSeparator();
+            var label1 = new Label { Text = house.Street,  FontFamily = "OpenSansRegular", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
+            var label2 = new Label { Text = house.City,  FontFamily = "OpenSansRegular", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
+            var label3 = new Label { Text = houseTypeTranslation,  FontFamily = "OpenSansRegular", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
+            var label4 = new Label { Text = "€"+house.Price,  FontFamily = "OpenSansRegular", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center};
             MatchRequests.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100, GridUnitType.Absolute) });
+            MatchRequests.Add(separator, 0, i);
+            Grid.SetColumnSpan(separator, 6);
             MatchRequests.Add(border, 0, i);
             MatchRequests.Add(label1, 1, i);
             MatchRequests.Add(label2, 2, i);
             MatchRequests.Add(label3, 3, i);
             MatchRequests.Add(label4, 4, i);
         }
-        AddLegend("Straat", "Stad", "Type","Prijs");
     }
 
     public void AddLegend(string label1, string label2, string label3, string label4)
     {
-        /*var image = new Image
-        {   Scale = 0.3,
-            Source = "arrowupdown.png",
-            Aspect = Aspect.Center,
-            HorizontalOptions = LayoutOptions.Center,
-            VerticalOptions = LayoutOptions.Center
-        };*/
-        
+        MatchRequests.RowDefinitions.Add(new RowDefinition { Height = new GridLength(40, GridUnitType.Absolute) });
         var columns = new List<Label>
         {
-            new Label { Text = label1, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center },
-            new Label { Text = label2, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center },
-            new Label { Text = label3, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center },
-            new Label { Text = label4, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center },
-            new Label { Text = "Match request", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center }
+            new Label { Text = label1, FontFamily= "InterBold", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center },
+            new Label { Text = label2, FontFamily= "InterBold",HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center },
+            new Label { Text = label3, FontFamily= "InterBold",HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center },
+            new Label { Text = label4, FontFamily= "InterBold",HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center },
+            new Label { Text = "Match request", FontFamily= "InterBold",HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center }
         };
         
         for (int i = 0; i < columns.Count; i++)
         {
             MatchRequests.Add(columns[i], i + 1, 0);
-            MatchRequests.Add(new Image
+            /*MatchRequests.Add(new Image
             {
                 Scale = 0.3,
                 Source = "arrowupdown.png",
                 Aspect = Aspect.AspectFit,
                 HorizontalOptions = LayoutOptions.End,
                 VerticalOptions = LayoutOptions.Center
-            }, i + 1, 0);
+            }, i + 1, 0);*/
+            var separator = CreateSeparator();
+            MatchRequests.Add(separator, 0, 0);
+            Grid.SetColumnSpan(separator, 6);
         }
+    }
+    
+    public void DisplayStatus(int row, status status)
+    {
+            var statusLabel = new Label
+            {
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.Center
+            };
+            var statusImage = new Image
+            {
+                Aspect = Aspect.AspectFit,
+                Scale = 0.2,
+                HorizontalOptions = LayoutOptions.End,
+                VerticalOptions = LayoutOptions.Center,
+                BackgroundColor = Colors.Transparent,
+            };
+            switch (status)
+            {
+                case status.Accepted:
+                    statusLabel.Text = "Geaccepteerd";
+                    statusImage.Source = "circleaccepted.png";
+                    break;
+                case status.Pending:
+                    statusLabel.Text = "In behandeling";
+                    statusImage.Source = "circlepending.png";
+                    break;
+                case status.Rejected:
+                    statusLabel.Text = "Geweigerd";
+                    statusImage.Source = "circlerejected.png";
+                    break;
+            }
+            var buttonContainer = new HorizontalStackLayout
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Spacing = 5,
+            };
+            MatchRequests.Add(statusImage, 5, row);
+            MatchRequests.Add(statusLabel, 5, row);
+    }
+    
+    private BoxView CreateSeparator()
+    {
+        return new BoxView
+        {
+            HeightRequest = 2,
+            BackgroundColor = Colors.LightGray,
+            HorizontalOptions = LayoutOptions.Fill,
+            VerticalOptions = LayoutOptions.End
+        };
     }
 
 }
