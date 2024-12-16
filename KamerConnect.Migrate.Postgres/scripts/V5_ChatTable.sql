46960b3f6c018e3f9d7c5b3f03d3147fe5ff423d
samberkhout <s1199368@student.windesheim.nl>
2024-12-16 12:27:47 +0100

    Create V5_ChatTable.sql

---
+++ KamerConnect.Migrate.Postgres/scripts/V5_ChatTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE chatmessages (
+                              id UUID DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
+                              match UUID REFERENCES matchrequests(id) not null ,
+                              message TEXT not null ,
+                              timestamp TIMESTAMP WITH TIME ZONE DEFAULT NOW() not null ,
+                              sender UUID REFERENCES person(id) not null 
+);
