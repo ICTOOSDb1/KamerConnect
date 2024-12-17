421fa4129dac5c77b5b5f3cae668b5ea1ced951f
samberkhout <s1199368@student.windesheim.nl>
2024-12-17 13:19:52 +0100

    Update ChatRepositoryTests.cs

--- KamerConnect.UnitTests/Services/ChatRepositoryTests.cs
+++ KamerConnect.UnitTests/Services/ChatRepositoryTests.cs
@@ -5,7 +5,7 @@ using KamerConnect.Services;
 using Moq;
 using Npgsql;
 
-namespace KamerConnect.UnitTests.Repository;
+namespace KamerConnect.UnitTests.Services;
 
 [TestFixture]
 public class ChatRepositoryTests
@@ -17,6 +17,21 @@ public class ChatRepositoryTests
     {
         _mockRepository = new Mock<IChatRepository>();
         _chatService = new ChatService(_mockRepository.Object);
+    }
+    [Test]
+    public void Create_WhenCalled_createMessage()
+    {
+        // Arrange
+        var newMessage = new ChatMessage(Guid.NewGuid(), Guid.NewGuid(), "test", DateTime.Now);
+        var chat = new Chat(Guid.NewGuid(), Guid.NewGuid());
+        _mockRepository.Setup(r => r.CreateMessage(newMessage, chat.ChatId));
+
+        // Act
+        _chatService.CreateMessage(newMessage, chat.ChatId);
+
+        // Assert
+       
+        _mockRepository.Verify(r => r.CreateMessage(newMessage, chat.ChatId), Times.Once);
     }
     
 }
