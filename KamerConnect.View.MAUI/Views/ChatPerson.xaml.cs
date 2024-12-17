6724467e10635991cb0c390eb59f41f44369cd39
Aernout5 <aernout.bergsma@live.nl>
2024-12-17 13:25:31 +0100

    chatpages toegevoegd

---
+++ KamerConnect.View.MAUI/Views/ChatPerson.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamerConnect.View.MAUI.Views;
+
+public partial class ChatPerson : ContentView
+{
+    public ChatPerson()
+    {
+        InitializeComponent();
+    }
+    
+    /*private void OnChatTapped(object sender, EventArgs e)
+    {
+        if (BindingContext is House house)
+        {
+            var housePage = MauiProgram.Services.GetRequiredService<HousePage>();
+            housePage.BindingContext = house;
+
+            App.Current.MainPage = new NavigationPage(housePage);
+        }
+    }*/
+}
