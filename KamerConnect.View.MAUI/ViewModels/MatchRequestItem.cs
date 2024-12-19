a0606fdc00b8af489294707e2475b1cd5941b6bc
niekvandenberg <niek2004@icloud.com>
2024-12-19 15:07:00 +0100

    chore: moved class to different file

---
+++ KamerConnect.View.MAUI/ViewModels/MatchRequestItem.cs
@@ -0,0 +1,115 @@
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using KamerConnect.Models;
+using KamerConnect.Services;
+using KamerConnect.Utils;
+using KamerConnect.View.MAUI.Pages;
+
+namespace KamerConnect.View.MAUI.ViewModels;
+
+public class MatchRequestItem
+{
+    private readonly ObservableCollection<MatchRequestItem> _parentCollection;
+
+    public string ProfilePicturePath { get; set; }
+    public string Field1 { get; set; }
+    public string Field2 { get; set; }
+    public string Field3 { get; set; }
+    public string Field4 { get; set; }
+    public string StatusText { get; set; }
+    public Color StatusColor { get; set; }
+    public bool ShowStatusButtons { get; set; }
+    public bool ShowRevertButton { get; set; }
+    public string RevertButtonText { get; set; }
+
+    public Match Match { get; set; }
+
+    public ICommand AcceptCommand { get; }
+    public ICommand RejectCommand { get; }
+    public ICommand RevertStatusCommand { get; }
+    public ICommand GoToPage { get; }
+
+    public MatchRequestItem(Person person, Match match, FileService fileService, ProfilePage profilePage,
+        ObservableCollection<MatchRequestItem> parentCollection)
+    {
+        _parentCollection = parentCollection;
+
+        Match = match;
+
+        ProfilePicturePath = person.ProfilePicturePath != null
+            ? fileService.GetFilePath("profilepictures", person.ProfilePicturePath)
+            : "user.png";
+        Field1 = string.Join(" ", [person.FirstName, person.MiddleName, person.Surname]);
+        Field2 = person.Gender.GetDisplayName();
+        Field3 = person.Personality?.Study ?? "";
+        Field4 = person.BirthDate.ToShortDateString();
+
+        ShowStatusButtons = true;
+        ShowRevertButton = false;
+
+        AcceptCommand = new Command(() => UpdateStatus(match, Status.Accepted));
+        RejectCommand = new Command(() => UpdateStatus(match, Status.Rejected));
+        
+        RevertStatusCommand = new Command(() => UpdateStatus(match, match.Status == Status.Accepted ? Status.Rejected : Status.Accepted));
+
+        
+        
+        if (match.Status != Status.Pending)
+        {
+            ShowRevertButton = true;
+            RevertButtonText = match.Status == Status.Accepted ? "Afwijzen" : "Accepteren";
+        }
+
+        GoToPage = new Command(() => { App.Current.MainPage = new NavigationPage(profilePage); });
+    }
+
+    public MatchRequestItem(House house, Match match, FileService fileService, HousePage housePage,
+        ObservableCollection<MatchRequestItem> parentCollection)
+    {
+        _parentCollection = parentCollection;
+
+        Match = match;
+
+        ProfilePicturePath = house.HouseImages[0].Path != null
+            ? house.HouseImages[0].FullPath
+            : "house.png";
+        Field1 = house.Street + " " + house.HouseNumber + house.HouseNumberAddition;
+        Field2 = house.City ?? "";
+        Field3 = house.Type.GetDisplayName() ?? "";
+        Field4 = "€" + house.Price;
+
+        ShowStatusButtons = false;
+
+        (StatusText, StatusColor) = GetStatusDisplay(match.Status);
+
+        GoToPage = new Command(() => { App.Current.MainPage = new NavigationPage(housePage); });
+    }
+
+    private static (string Text, Color Color) GetStatusDisplay(Status status) =>
+        status switch
+        {
+            Status.Accepted => (Status.Accepted.GetDisplayName(), Colors.Green),
+            Status.Pending => (Status.Pending.GetDisplayName(), Colors.Orange),
+            Status.Rejected => (Status.Rejected.GetDisplayName(), Colors.Red),
+            _ => ("", Colors.Gray)
+        };
+
+    private void UpdateStatus(Match match, Status status)
+    {
+        var matchService = MauiProgram.Services.GetRequiredService<MatchService>();
+        matchService.UpdateStatusMatch(match, status);
+        if (match.Status != status)
+        {
+            _parentCollection.Remove(this);
+            match.Status = status;
+            
+            if (match.Status != Status.Pending)
+            {
+                ShowRevertButton = true;
+                RevertButtonText = match.Status == Status.Accepted ? "Afwijzen" : "Accepteren";
+            }
+        }
+        
+        
+    }
+}
